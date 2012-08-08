2a75daffce5f7c40c08c2ea5cdf0e2aedc445959
Dean Rather <deanrather@gmail.com>
2012-08-08 17:50:00 +1000

    plupload chunking & use config for directories

--- Plupload.php
+++ Plupload.php
@@ -21,8 +21,13 @@ namespace application\plugin\plupload
 				throw new PluploadException(PluploadException::MUST_HAVE_DATA, $_SERVER);
 			}
 			
+			$config = Nutshell::getInstance()->config;
+			$temporary_dir = $config->plugin->Plupload->temporary_dir;
+			$completed_dir = $config->plugin->Plupload->completed_dir;
+			$thumbnail_dir = $config->plugin->Plupload->thumbnail_dir;
+			
 			// "configure" the upload directory
-			$targetDir = 'c:\\tmp\\';
+			$targetDir = $temporary_dir;
 			
 			// Run Pluploads's uploader script
 			include(__DIR__._DS_.'thirdparty'._DS_.'plupload.php');
